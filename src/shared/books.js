be17e3a4eb054f71a64405c3be3b0cc23c5badb4
betsystevens <betsystevens5@gmail.com>
2020-08-04 23:25:55 -0500

    add images add book data

---
+++ src/shared/books.js
@@ -0,0 +1,63 @@
+export const BOOKS = [
+  {
+    id: 0,
+    img: "changing.jpg",
+    title: "Changing Stuff and Seeing What Happens",
+    subtitle: "How to actually learn any new programming concept"
+  },
+  {
+    id: 1,
+    img: "copyhorse.jpg",
+    title: "Copy and Pasting from Stackoverflow",
+    subtitle: "Asking yourself why you didn't think of that"
+  },
+  {
+    id: 2,
+    img: "excuses.jpg",
+    title: "Expert Excuses for Not Writing Unit Tests",
+    subtitle: "Your application is a special snowflake"
+  },
+  {
+    id: 3,
+    img: "deleting.jpg",
+    title: "Deleting Code",
+    subtitle: "Probably the most satisfying part of your job"
+  },
+  {
+    id: 4,
+    img: "agile.jpg",
+    title: "Agile Management",
+    subtitle: "Put the 'agile' in fragile"
+  },
+  {
+    id: 5,
+    img: "blaming.jpg",
+    title: "Blaming the User",
+    subtile: "You're a 10x hacker and it must be someone else's fault"
+  },
+  {
+    id: 6,
+    img: "buzzword.jpg",
+    title: "Buzzword-first Design",
+    subtile: "Fashion-forward Developement"
+  },
+  {
+    id: 7,
+    img: "calling.jpb",
+    title: "Calling a bug a feature",
+    subtile: "We'll fix it later"
+  },
+  {
+    id: 8,
+    img: "coding.jpg",
+    title: "Coding on the Weekend",
+    subtile: "It's like weekday coding, but pants are optional"
+  },
+  {
+    id: 9,
+    img: "getting.jpg",
+    title: "Getting Around to Security Next Month",
+    subtile: "Putting off critical tasks until everyone forgets about them"
+  },
+
+]
