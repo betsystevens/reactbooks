0899429b3e8138e6acafc79c556dea31408c9f20
betsystevens <betsystevens5@gmail.com>
2020-08-05 22:10:21 -0500

    add books

--- src/shared/books.js
+++ src/shared/books.js
@@ -3,60 +3,119 @@ export const BOOKS = [
     id: 0,
     image: "assets/images/changing.jpg",
     title: "Changing Stuff and Seeing What Happens",
-    subtitle: "How to actually learn any new programming concept"
+    subtitle: "How to actually learn any new programming concept",
+    price: "$28.00"
   },
   {
     id: 1,
     image: "assets/images/copyhorse.jpg",
-    title: "Copy and Pasting from Stackoverflow",
-    subtitle: "Asking yourself why you didn't think of that"
+    title: "Copy and Pasting from Stack Overflow",
+    subtitle: "Asking yourself why you didn't think of that",
+    price: "$38.00"
   },
   {
     id: 2,
     image: "assets/images/excuses.jpg",
     title: "Expert Excuses for Not Writing Unit Tests",
-    subtitle: "Your application is a special snowflake"
+    subtitle: "Your application is a special snowflake",
+    price: "$42.00"
   },
   {
     id: 3,
     image: "assets/images/deleting.jpg",
     title:"Deleting Code",
-    subtitle: "Probably the most satisfying part of your job"
+    subtitle: "Probably the most satisfying part of your job",
+    price: "$28.00"
   },
   {
     id: 4,
     image: "assets/images/agile.jpg",
     title: "Agile Management",
-    subtitle: "Put the 'agile' in fragile"
+    subtitle: "Put the 'agile' in fragile",
+    price: "$32.00"
   },
   {
     id: 5,
     image: "assets/images/blaming.jpg",
     title: "Blaming the User",
-    subtitle: "You're a 10x hacker and it must be someone else's fault"
+    subtitle: "You're a 10x hacker and it must be someone else's fault",
+    price: "$35.00"
   },
   {
     id: 6,
     image: "assets/images/buzzword.jpg",
     title: "Buzzword-first Design",
-    subtitle: "Fashion-forward Developement"
+    subtitle: "Fashion-forward Development",
+    price: "$64.00"
   },
   {
     id: 7,
     image: "assets/images/calling.jpg",
-    title: "Calling a bug a feature",
-    subtitle: "We'll fix it later"
+    title: "Calling a Bug a Feature",
+    subtitle: "We'll fix it later",
+    price: "$32.00"
   },
   {
     id: 8,
     image: "assets/images/coding.jpg",
     title: "Coding on the Weekend",
-    subtitle: "It's like weekday coding, but pants are optional"
+    subtitle: "It's like weekday coding, but pants are optional",
+    price: "$35.00"
   },
   {
     id: 9,
     image: "assets/images/getting.jpg",
     title: "Getting Around to Security Next Month",
-    subtitle: "Putting off critical tasks until everyone forgets about them"
-  }
+    subtitle: "Putting off critical tasks until everyone forgets about them",
+    price: "$64.00"
+  },
+  {
+    id: 10,
+    image: "assets/images/copy.jpg",
+    title: "Copy and Pasting from Stack Overflow",
+    subtitle: "Cutting Corners to Meet Arbitrary Management Deadlines",
+    price: "$21.00"
+  },
+  {
+    id: 11,
+    image: "assets/images/date.jpb",
+    title: "Date and Time in JavaScript",
+    subtitle: "Book 1 of 6",
+    price: "$72.00"
+  },
+  {
+    id: 12,
+    image: "assets/images/googling.jpg",
+    title: "Googling the Error Message",
+    subtitle: "The Internet Will Make Those Bad Words Go Away",
+    price: "$12.00"
+  },
+  {
+    id: 13,
+    image: "assets/images/halflisten.jpg",
+    title: "Half-listening to Conference Talks",
+    subtitle: "Pay $1,500 to Browse Twitter and Hang-out on Slack",
+    price: "$88.00"
+  },
+  {
+    id: 14,
+    image: "assets/images/hoping",
+    title: "Hoping This Works",
+    subtitle: "Solutions that might fix the problem without breaking anything",
+    price: "$56.00"
+  },
+  {
+    id: 15,
+    image: "assets/images/managing",
+    title: "Navigating and Managing One Billion Browser Tabs",
+    subtitle: "Because you know you just saw the tab you need",
+    price: "$11.00"
+  },
+  {
+    id: 16,
+    image: "assets/images/memorizing",
+    title: "Memorizing Six Git Commands",
+    subtitle: "The Popular Approach to Version Control",
+    price: "$29.00"
+  },
 ]
