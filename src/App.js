53421caa542bf20b0c76fa16eeab08f6c999f8f9
betsystevens <betsystevens5@gmail.com>
2020-08-05 22:27:16 -0500

    remove data from ListBooks component, import data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import ListBooks from './components/ListBooks'
+import ListBooks from './components/ListBooks';
+import { BOOKS } from './shared/books';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: BOOKS
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">O'Really Books</NavbarBrand>
           </div>
         </Navbar> 
-        <ListBooks />
+        <ListBooks books={this.state.books} />
       </div> 
     )
   }
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,73 +1,17 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class ListBooks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [
-        {
-          id: 0,
-          image: "assets/images/changing.jpg",
-          title: "Changing Stuff and Seeing What Happens",
-          subtitle: "How to actually learn any new programming concept"
-        },
-        {
-          id: 1,
-          image: "assets/images/copyhorse.jpg",
-          title: "Copy and Pasting from Stackoverflow",
-          subtitle: "Asking yourself why you didn't think of that"
-        },
-        {
-          id: 2,
-          image: "assets/images/excuses.jpg",
-          title: "Expert Excuses for Not Writing Unit Tests",
-          subtitle: "Your application is a special snowflake"
-        },
-        {
-          id: 3,
-          image: "assets/images/deleting.jpg",
-          title:"Deleting Code",
-          subtitle: "Probably the most satisfying part of your job"
-        },
-        {
-          id: 4,
-          image: "assets/images/agile.jpg",
-          title: "Agile Management",
-          subtitle: "Put the 'agile' in fragile"
-        },
-        {
-          id: 5,
-          image: "assets/images/blaming.jpg",
-          title: "Blaming the User",
-          subtitle: "You're a 10x hacker and it must be someone else's fault"
-        },
-        {
-          id: 6,
-          image: "assets/images/buzzword.jpg",
-          title: "Buzzword-first Design",
-          subtitle: "Fashion-forward Developement"
-        },
-        {
-          id: 7,
-          image: "assets/images/calling.jpg",
-          title: "Calling a bug a feature",
-          subtitle: "We'll fix it later"
-        },
-        {
-          id: 8,
-          image: "assets/images/coding.jpg",
-          title: "Coding on the Weekend",
-          subtitle: "It's like weekday coding, but pants are optional"
-        },
-        {
-          id: 9,
-          image: "assets/images/getting.jpg",
-          title: "Getting Around to Security Next Month",
-          subtitle: "Putting off critical tasks until everyone forgets about them"
-        }
-      ]
+      selectedBook: null
     }
+  }
+
+  onBookSelected(book) {
+    this.setState({ selectedBook: book })
   }
 
   render() {
@@ -79,7 +23,7 @@ class ListBooks extends Component {
       fontStyle: 'italic',
       fontSize: 20 
     }
-    const booklist = this.state.books.map((book) => {
+    const booklist = this.props.books.map((book) => {
       return (
         <div key={book.id} className="col-12 mt-5">
           <Media className="border-bottom" tag="li">
